1a357cdcb6b204bb7df147461b16d1cc15b05143
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-07-14 17:25:48 -0400

    add filter capability

--- proboscis.py
+++ proboscis.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from datetime import datetime
 
@@ -8,10 +9,18 @@ db = pymongo.Connection().mongolog
 time_key = 'created'
 message_key = 'msg'
 
-last_time = list(db.log.find({}, [time_key]).sort(time_key, pymongo.DESCENDING).limit(1))[0][time_key]
+if len(sys.argv) > 1:
+    filter_query = eval(sys.argv[1])
+else:
+    filter_query = {}
+
+last_time = list(db.log.find(filter_query, [time_key]).sort(time_key, pymongo.DESCENDING).limit(1))[0][time_key]
 
 while True:
-    for row in db.log.find({time_key: {'$gt': last_time}}).sort(time_key, pymongo.ASCENDING):
+    query = {time_key: {'$gt': last_time}}
+    query.update(filter_query)
+    
+    for row in db.log.find(query).sort(time_key, pymongo.ASCENDING):
         message = row.get(message_key, None)
         if message:
             print datetime.fromtimestamp(float(row[time_key])).strftime('%Y-%m-%d %H:%M:%S.%f:\t'),
