e7aa5096309ff78264af09f97576a7ab4b90565e
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-07-16 10:54:19 -0400

    factor out mongodb info into variables

--- proboscis.py
+++ proboscis.py
@@ -4,23 +4,25 @@ from datetime import datetime
 
 import pymongo
 
-db = pymongo.Connection().mongolog
-
+db_name = 'mongolog'
+collection_name = 'log'
 time_key = 'created'
 message_key = 'msg'
+
+db = pymongo.Connection()[db_name]
 
 if len(sys.argv) > 1:
     filter_query = eval(sys.argv[1])
 else:
     filter_query = {}
 
-last_time = list(db.log.find(filter_query, [time_key]).sort(time_key, pymongo.DESCENDING).limit(1))[0][time_key]
+last_time = list(db[collection_name].find(filter_query, [time_key]).sort(time_key, pymongo.DESCENDING).limit(1))[0][time_key]
 
 while True:
     query = {time_key: {'$gt': last_time}}
     query.update(filter_query)
     
-    for row in db.log.find(query).sort(time_key, pymongo.ASCENDING):
+    for row in db[collection_name].find(query).sort(time_key, pymongo.ASCENDING):
         message = row.get(message_key, None)
         if message:
             print datetime.fromtimestamp(float(row[time_key])).strftime('%Y-%m-%d %H:%M:%S.%f:\t'),
